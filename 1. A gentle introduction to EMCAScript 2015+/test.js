075cf03af1ef821e0d014abad5ff84bd0c28bc8d
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2026-05-08 18:18:07 +0545

    Introduction to ECMAScript 2015+ added

--- 1. A gentle introduction to EMCAScript 2015+/test.js
+++ 1. A gentle introduction to EMCAScript 2015+/test.js
@@ -62,3 +62,74 @@ console.log(naam, umer );                          // Philip 26
 const timeouts = [1000, 2000, 3000];
 const [shortTimeout, mediumTimeout] = timeouts;    // we now have variables 'shortTimeout' and 'mediumTimeout'
 console.log(shortTimeout, mediumTimeout);          // 1000, 2000
+
+
+//  9. Classes ----------------------------------------------------------------------
+class Pony {
+    constructor(color) {
+        this.color = color;
+    }
+
+        toString() {
+        return `${this.color} pony`;
+        // see that? It is another cool feature of ES2015, called template literals
+        // we'll talk about these quickly!
+    }
+}
+
+const bluePony = new Pony('blue');
+console.log(bluePony.toString()); // blue pony
+
+class Phone {
+    constructor (price) {
+        this.price = price;
+    }
+}
+
+class Apple extends Phone {
+    constructor (price, color) {
+        super(price);           // calling the base class constructor with 'super'
+        this.color = color;
+    }
+}
+const apple = new Apple(200, 'gray');
+console.log(apple.price);       // 200
+console.log(apple.color);       // gray
+
+
+//  Promise ------------------------------------------------------------------------------
+// creating promise 
+let p = new Promise(function(resolve, reject) {
+    let i = 1+1;
+    if(i == 2){
+        resolve('success')
+    }
+    else{
+        reject('failed')        
+    }
+});
+
+// using the promise
+p.then(function(message) {
+    console.log('This is in the then ' + message);
+}).catch(function(message) {
+    console.log('This is in the reject ' + message);
+})
+
+
+//  Aroow function --------------------------------------------------------------------
+var maxFinder = {
+    max: 0,
+    find: function (numbers) {
+        // let's iterate
+        numbers.forEach(function (element) {
+            // if the element is greater, set it as the max
+            if (element > this.max) {
+                this.max = element;
+            }
+        });
+    }
+};
+maxFinder.find([2, 3, 4]);
+// log the result
+console.log(maxFinder.max); // expected result 4 but it doesn't work as expected
