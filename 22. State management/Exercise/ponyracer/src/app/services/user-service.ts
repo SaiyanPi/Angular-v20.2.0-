5e0666b902029c6a39a4c4947376a81909b27946
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2026-06-25 10:42:46 +0545

    Exercise- Remember me added for a chapter 22: state management. Now i can proceed with the testing of  exercise - HTTP with authentication in chapter 17: Http

--- 22. State management/Exercise/ponyracer/src/app/services/user-service.ts
+++ 22. State management/Exercise/ponyracer/src/app/services/user-service.ts
@@ -1,15 +1,32 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { effect, inject, Injectable, signal, untracked } from '@angular/core';
 import { UserModel } from '../models/user-model';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 
+const USER_LOCAL_STORAGE_KEY = 'rememberMe';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
   private readonly http = inject(HttpClient);
-  private readonly user = signal<UserModel | undefined>(undefined);
+  private readonly user = signal<UserModel | undefined>(this.retrieveUser());
   readonly currentUser = this.user.asReadonly();
+
+  constructor() {
+    effect(() => {
+      // every time the user signal changes, we store it in local storage
+      const user = this.user();
+      untracked(() => {
+        if (user) {
+          window.localStorage.setItem(USER_LOCAL_STORAGE_KEY, JSON.stringify(user));
+        } else {
+          window.localStorage.removeItem(USER_LOCAL_STORAGE_KEY);
+        }
+      });
+    })
+  }
 
   authenticate(login: string, password: string): Observable<UserModel> {
     return this.http
@@ -24,5 +41,13 @@ export class UserService {
 
   logout(): void {
     this.user.set(undefined);
+  }
+
+  private retrieveUser(): UserModel | undefined {
+    const value = window.localStorage.getItem(USER_LOCAL_STORAGE_KEY);
+    if (value) {
+      return JSON.parse(value) as UserModel;
+    }
+    return undefined;
   }
 }
